1f39fbd2c795d72b9f7b8f084a1d2e5232073000
James Macon <jtmacon36@gmail.com>
2015-11-26 11:27:32 -0500

    load-in of current work

---
+++ js/jmapp.js
@@ -0,0 +1,18 @@
+/*
+	@author James Macon
+	@version 0.1
+*/
+
+var jmapp = jmApp || {};
+
+//Site Dependencies
+var dependencies = [
+	{jquery		: "helpers/jquery.2.1.4.min.js"},
+	{instagram	: "helpers/instafeed.min.js"},
+	{velocity	: "helpers/velocity.min.js"}
+];
+
+jmapp.init = (function()
+{
+	var instagram 
+})();
